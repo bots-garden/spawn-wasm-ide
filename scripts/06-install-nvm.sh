10ab700e6185d272ced467280f76827d021e82ff
Philippe Charrière <ph.charriere@gmail.com>
2022-12-27 16:49:04 +0100

    💾 Saved.

--- scripts/06-install-nvm.sh
+++ scripts/06-install-nvm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -o allexport; source config/.env; set +o allexport
 #sudo snap install node --classic --channel=19
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v${NVM_VERSION}/install.sh | bash
 source $HOME/.bashrc
-nvm install 18.11
-#nvm use 18.11
+nvm install ${NODEJS_VERSION}
+#nvm use ${NODEJS_VERSION}
